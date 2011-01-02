f31023e5177053abb6fcb99daff0ea23ee177bf1
Brian Goslinga <gosli008@morris.umn.edu>
2011-01-01 23:42:25 -0600

    Move version to 1.0.0-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject syntax-rules "1.0.0-SNAPSHOT"
-  :description "A Clojure-flavored syntax-rules implementation for Clojure"
+(defproject org.clojars.qbg/syntax-rules
+  "1.0.0-alpha1"
+  :description "syntax-rules/syntax-case/syntax-parse mashup for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
