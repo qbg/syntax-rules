df5ab02824dc6f233afbe5370e98a4e68aafe988
Brian Goslinga <gosli008@morris.umn.edu>
2011-01-10 23:50:10 -0600

    Bump version to 1.0.0-alpha2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.qbg/syntax-rules
-  "1.0.0-SNAPSHOT"
+  "1.0.0-alpha2"
   :description "syntax-rules/syntax-case/syntax-parse mashup for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
